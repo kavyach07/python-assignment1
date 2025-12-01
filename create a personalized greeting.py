0ad42f9eb42b1e2f36e1366c6143b9e2f5149853
kavyach07 <149611853+kavyach07@users.noreply.github.com>
2025-12-02 01:11:08 +0530

    Add files via upload

---
+++ create a personalized greeting.py
@@ -0,0 +1,6 @@
+
+name1=input("enter your first name :")
+name2=input("enter your last name :")
+fullname=name1 + " "  + name2
+print( "Hello,", fullname,"!")
+print("Wellcome to the python class.")
